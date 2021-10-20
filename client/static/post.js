4bc2c448652e2899002ff50d590ce007a12dcb84
“yourGithubUsername” <your@emailadd.com>
2021-10-20 22:22:54 +0100

    fix bugs in models and server

--- client/static/post.js
+++ client/static/post.js
@@ -1,28 +1,23 @@
-const url = window.location.href;
-
-const postTitle = document.getElementById('postTitle');
-const postName = document.getElementById('postName');
-const postStory = document.getElementById('postStory');
-
+let url = window.location.href;
 if(url.includes('#')) {
-    const id = urlCon.split('#').pop();
+    const id = url.split('#').pop(); 
     renderPost(id);
 } else {
-    let publishForm = document.getElementById('publishForm');
-    publishForm.addEventListener('submit', postContent)
+    let form = document.getElementById('postForm');
+    form.addEventListener('submit', postData)
 }
 
-async function postContent(e) {
+async function postData(e) {
     e.preventDefault();
-    const postContent = {
+    const postData = {
         title: e.target.postTitle.value,
-        name: e.target.postName.value,
-        story: e.target.postStory.value
+        author: e.target.postAuthor.value,
+        content: e.target.postContent.value
     };
 
-    const options = {
+    const options = { 
         method: 'POST',
-        body: JSON.stringify(postContent),
+        body: JSON.stringify(postData),
         headers: { "Content-Type": "application/json" }
     };
 
@@ -31,54 +26,158 @@ async function postContent(e) {
 
     window.location.replace(`/#${json.post}`);
     location.reload();
+
 };
 
-async function getContent(id) {
+async function getData(id) {
     const data = await fetch(`http://localhost:3000/${id}`)
     const json = await data.json()
     return json
 }
 
 async function renderPost(id) {
-    const bodyContent = document.getElementById('bodyContent');
-    bodyContent.textContent = ''
-    let postContent
-    
-    const date = new Date(postContent.date * 1000)
+
+    const section = document.getElementById('main-content');
+    section.textContent = ''
+    let postData
+
+    try {
+        postData = await getData(id)
+        if(postData.message) {
+            throw new Error ('Error returned')
+        }
+        postData = postData.post
+    } catch (err) {
+        return(err)
+    }
+
+    const date = new Date(postData.date * 1000)
 
     const post = document.createElement('div')
     const postTitle = document.createElement('h1')
-    const postName = document.createElement('h3')
+
+    const postMeta = document.createElement('address')
+    const postAuthor = document.createElement('a')
     const postDate = document.createElement('time')
-    const postStory = document.createElement('p')
-    
+
+    const postContent = document.createElement('p')
+
     post.classList.add('container')
-    post.classList.add('flex-columm')
+    post.classList.add('flex-column')
 
+    postDate.setAttribute('datetime',date.toISOString()) 
 
-    postDate.setAttribute('datetime', date.toISOString())
-
-    postTitle.textContent = postContent.title
-    postName.textContent = postContent.name
-    postName.rel = "name"
+    postTitle.textContent = postData.title
+    postAuthor.textContent = postData.author
+    postAuthor.rel = 'Author'
     postDate.textContent = formatDate(date)
-    postStory.textContent = postContent.content
+    postContent.textContent = postData.content
 
-    post.appendChild(postDate)
+    postMeta.append(postAuthor)
+    postMeta.append(postDate)
+
     post.appendChild(postTitle)
-    post.appendChild(postName)
-    post.appendChild(postStory)
+    post.appendChild(postMeta)
+    post.appendChild(postContent)
 
-    bodyContent.appendChild(post)
+    section.appendChild(post)
     
 }
 
-function formateDate(date) {
-    const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const month = months[date.getMonth()];
+function formatDate(date) {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[date.getMonth()];
     const day = String(date.getDate()).padStart(2, '0');
     const year = date.getFullYear();
-    const output = day + `\n` + month + ',' + year;
+    const output = day  + '\n'+ month  + ',' + year;
 
     return output
 }
+
+// const url = window.location.href;
+
+// const postTitle = document.getElementById('postTitle');
+// const postName = document.getElementById('postName');
+// const postStory = document.getElementById('postStory');
+
+// if(url.includes('#')) {
+//     const id = urlCon.split('#').pop();
+//     renderPost(id);
+// } else {
+//     let publishForm = document.getElementById('publishForm');
+//     publishForm.addEventListener('submit', postContent)
+// }
+
+// async function postContent(e) {
+//     e.preventDefault();
+
+
+//     const postContent = {
+//         title: e.target.postTitle.value,
+//         name: e.target.postName.value,
+//         story: e.target.postStory.value
+//     };
+
+//     const options = {
+//         method: 'POST',
+//         body: JSON.stringify(postContent),
+//         headers: { "Content-Type": "application/json" }
+//     };
+
+//     const response = await fetch('http://localhost:3000/', options)
+//     const json = await response.json()
+
+//     window.location.replace(`/#${json.post}`);
+//     location.reload();
+// };
+
+// async function getContent(id) {
+//     const data = await fetch(`http://localhost:3000/${id}`)
+//     const json = await data.json()
+//     return json
+// }
+
+// async function renderPost(id) {
+//     const bodyContent = document.getElementById('bodyContent');
+//     bodyContent.textContent = ''
+//     let postContent
+    
+//     const date = new Date(postContent.date * 1000)
+
+//     const post = document.createElement('div')
+//     const postTitle = document.createElement('h1')
+//     const postName = document.createElement('h3')
+//     const postDate = document.createElement('time')
+//     const postStory = document.createElement('p')
+    
+//     post.classList.add('container')
+//     post.classList.add('flex-columm')
+
+
+//     postDate.setAttribute('datetime', date.toISOString())
+
+//     postTitle.textContent = postContent.title
+//     postName.textContent = postContent.name
+//     postName.rel = "name"
+//     postDate.textContent = formatDate(date)
+//     postStory.textContent = postContent.content
+
+//     post.appendChild(postDate)
+//     post.appendChild(postTitle)
+//     post.appendChild(postName)
+//     post.appendChild(postStory)
+
+//     bodyContent.appendChild(post)
+    
+// }
+
+// function formateDate(date) {
+//     const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+//     const month = months[date.getMonth()];
+//     const day = String(date.getDate()).padStart(2, '0');
+//     const year = date.getFullYear();
+//     const output = day + `\n` + month + ',' + year;
+
+//     return output
+// }
--- server/models/post.js
+++ server/models/post.js
@@ -1,61 +1,120 @@
-const { init } = require('../dbConfig');
-const { ObjectId } = require('mongodb');
-
+const { init } = require ('../dbConfig')
+const { ObjectId } = require('mongodb')
 
 class Post {
 
-    constructor(data) {
-        this.title = data.title;
-        this.lowerCaseTitle = data.title.toLowerCase();
-        this.name = data.name;
-        this.story = data.story;
-        this.date = Math.floor(date.getTime() / 1000);
-        this.day = date.getDate();
-        this.month = date.getMonth();
-        this.url = `${data.title.replace(/ /g, "-").toLowerCase()}-${date.getDate()}-${date.getMonth() + 1}`
+    constructor(data){
+
+        const date = new Date()
+
+        this.title = data.title
+        this.lowerCaseTitle = data.title.toLowerCase()
+        this.author = data.author
+        this.content = data.content
+        this.date = Math.floor(date.getTime() / 1000)
+        this.day = date.getDate()
+        this.month = date.getMonth()
+        this.url = `${data.title.replace(/ /g,"-").toLowerCase()}-${date.getDate()}-${date.getMonth() + 1}` 
+        
     }
 
-    static create({title, name, story}) {
-        
+    static create({title, author, content}){
         return new Promise (async (resolve, reject) => {
-
             try {
-                const db = await init();
+                const db = await init();                
                 let lowerTitle = title.toLowerCase();
                 let newPost = new Post({
                     title,
-                    name,
-                    story
+                    author,
+                    content 
                 });
-
                 let urlCount = await db.collection('posts').find({ lowerCaseTitle: lowerTitle, day: newPost.day, month: newPost.month }).count();
-                if (urlCount > 0) {
+                if (urlCount > 0){
                     newPost.url += `-${urlCount+1}`
                 }
-
-                let postContent = await db.collection('posts').insertOne(newPost)
-                resolve(newPost.url);
-
-
-            } catch(err) {
+                let postData = await db.collection('posts').insertOne(newPost)
+                resolve (newPost.url);
+            } catch (err) {
                 reject(err);
             }
         });
     }
 
-
-
-    static findById(id) {
+    static findById (id) {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init();
-                let postContent = await db.collection('posts').find({ url:id }).toArray()
-                let post = new Post({...postContent[0], id: postContent[0]._id});
+                let postData = await db.collection('posts').find({ url: id }).toArray()
+                let post = new Post({...postData[0], id: postData[0]._id});
                 resolve (post);
             } catch (err) {
-                reject('Post does not exist');
+                reject('Post not found');
             }
-        })
+        });
     }
+
 }
-module.exports = Post
+
+module.exports = Post;
+
+//const { init } = require('../dbConfig');
+// const { ObjectId } = require('mongodb');
+
+
+// class Post {
+
+//     constructor(data) {
+//         this.title = data.title;
+//         this.lowerCaseTitle = data.title.toLowerCase();
+//         this.name = data.name;
+//         this.story = data.story;
+//         this.date = Math.floor(date.getTime() / 1000);
+//         this.day = date.getDate();
+//         this.month = date.getMonth();
+//         this.url = `${data.title.replace(/ /g, "-").toLowerCase()}-${date.getDate()}-${date.getMonth() + 1}`
+//     }
+
+//     static create({title, name, story}) {
+        
+//         return new Promise (async (resolve, reject) => {
+
+//             try {
+//                 const db = await init();
+//                 let lowerTitle = title.toLowerCase();
+//                 let newPost = new Post({
+//                     title,
+//                     name,
+//                     story
+//                 });
+
+//                 let urlCount = await db.collection('posts').find({ lowerCaseTitle: lowerTitle, day: newPost.day, month: newPost.month }).count();
+//                 if (urlCount > 0) {
+//                     newPost.url += `-${urlCount+1}`
+//                 }
+
+//                 let postContent = await db.collection('posts').insertOne(newPost)
+//                 resolve(newPost.url);
+
+
+//             } catch(err) {
+//                 reject(err);
+//             }
+//         });
+//     }
+
+
+
+//     static findById(id) {
+//         return new Promise (async (resolve, reject) => {
+//             try {
+//                 const db = await init();
+//                 let postContent = await db.collection('posts').find({ url:id }).toArray()
+//                 let post = new Post({...postContent[0], id: postContent[0]._id});
+//                 resolve (post);
+//             } catch (err) {
+//                 reject('Post does not exist');
+//             }
+//         })
+//     }
+// }
+// module.exports = Post
